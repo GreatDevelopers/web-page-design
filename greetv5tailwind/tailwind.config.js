fb0ede33e31a88ab57f28b15ef0ed92f1d359b4a
SUKHPREET SINGH <82471879+sukhlotey@users.noreply.github.com>
2025-02-01 01:58:50 +0530

    added tailwindcss

---
+++ greetv5tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Include your main HTML file
+    "./js/**/*.js", // Include JavaScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
